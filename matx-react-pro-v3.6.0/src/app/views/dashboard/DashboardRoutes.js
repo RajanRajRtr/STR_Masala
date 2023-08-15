367462cbcd1f3cdb703db057451e7e353ae6db78
satheeshtec <satheesh.s@legendstechsolution.com>
2023-08-15 13:28:03 +0530

    add an backend

--- matx-react-pro-v3.6.0/src/app/views/dashboard/DashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import { authRoles } from "app/auth/authRoles";
-import Analytics2 from "./Analytics2"
-
-const dashboardRoutes = [
-  { path: "dashboard/default", element: <Analytics2 />, auth: authRoles.sa },
-
-];
-
-export default dashboardRoutes;
